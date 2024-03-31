80920c6d7d742a393b305d06a0ae6fc2ab00497f
hoxinzhen <hoxinzhen@gmail.com>
2024-03-31 23:45:11 +0800

    home + layout

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 // cursor
 $(document).ready(function() {
+
+  // MOUSE MOVEMENT
   $(document).on('mousemove', function(e) {
     var cursorExtWidth = $('#cursor-ext').width();
     var cursorWidth = $('#cursor').width();
 
     $('#cursor').css({
-      left: e.pageX + cursorExtWidth/2 -cursorWidth/2.5,
-      top: e.pageY + cursorExtWidth/2 -cursorWidth/2.5,
+      left: e.clientX + cursorExtWidth/2 -cursorWidth/2.5,
+      top: e.clientY + cursorExtWidth/2 -cursorWidth/2.5,
     });
     $('#cursor-ext').css({
-      left: e.pageX,
-      top: e.pageY  
+      left: e.clientX,
+      top: e.clientY  
     });
   })
+
+  // HOVER
+  $(document).on('mouseover', '.hover', function(e) {
+    $('#cursor-ext').css({
+      width: '40px',
+      height: '40px'
+    });
+  });
+
+  $(document).on('mouseout', '.hover', function(e) {
+    $('#cursor-ext').css({
+      width: '25px',
+      height: '25px'
+    });
+  });
 
 });
 
