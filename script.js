f3492e796897ce2d20e58570a55a00f26b6ccbb2
Ho Xin Zhen <hoxinzhen@gmail.com>
2024-04-11 17:03:49 +0800

    cursor fix

--- script.js
+++ script.js
@@ -3,28 +3,25 @@ $(document).ready(function() {
 
   // MOUSE MOVEMENT
   $(document).on('mousemove', function(e) {
-    var cursorExtWidth = $('#cursor-ext').width();
-    var cursorWidth = $('#cursor').width();
-
     $('#cursor').css({
-      left: e.clientX + cursorExtWidth/2 -cursorWidth/2.5,
-      top: e.clientY + cursorExtWidth/2 -cursorWidth/2.5,
+      left: e.clientX,
+      top: e.clientY,
     });
     $('#cursor-ext').css({
       left: e.clientX,
-      top: e.clientY  
+      top: e.clientY,
     });
   })
 
   // HOVER
-  $(document).on('mouseover', '.hover', function(e) {
+  $(document).on('mouseenter', '.hover', function(e) {
     $('#cursor-ext').css({
       width: '40px',
       height: '40px'
     });
   });
 
-  $(document).on('mouseout', '.hover', function(e) {
+  $(document).on('mouseleave', '.hover', function(e) {
     $('#cursor-ext').css({
       width: '25px',
       height: '25px'
